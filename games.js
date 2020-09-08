3214c4ad342a6d83992ea3fa2a57cd322e807a7d
Ramona <drvizitiuramona@gmail.com>
2020-09-08 23:50:41 +0300

    final1

--- games.js
+++ games.js
@@ -1,91 +1,94 @@
-// URL object helps to work with search paramameters
-var baseUrl = new URL("https://games-world.herokuapp.com/games");
 
-function getActivty() {
-setTypeFilter();
-setParticipantsFilter();
+//VARIANTA 2 aici imi da erore [object object]
+var baseUrl ="https://games-world.herokuapp.com/games";
 
-    fetch(baseUrl.href, {method: "GET"})
-    .then(
-        function(response){
-            // parsing the response
-            return response.json();
-        }
-    ).then(
-        function(jsonResp) {
-            console.log(jsonResp);
-            // todo: handle no activity use case
-            if(jsonResp.error){
-                displayError(jsonResp.error)
-            }else{
-            displayActivity(jsonResp);
-             }
-        }
-    ).catch(function(error) {
-        console.log(error);
-        displayError(error);
-    })
+async function getGames() {
+
+let response=await fetch (baseUrl);
+let json=await response.json();
+console.log(json)
+
+
+var GamesElement=document.querySelector(".games");
+
+
+//<div class="games">   
+
+//<div class="list">
+//<div class="_id">id</div>
+    //<div class="title">title</div>
+        //<div class="gameImage">
+           // <img class="imageUrl" src= ""alt="game"></img>
+        //</div>
+   // <div class="description">Black hot d.....</div>
+//</div>
+//</div>
+
+json.forEach( Games=> {
+
+ const listElement=document.createElement("div");
+ listElement.classList.add("list");
+ GamesElement.appendChild(listElement);
+
+ const idElement=document.createElement("div");
+ idElement.classList.add("_id");
+ listElement.appendChild(idElement);
+
+ const titleElement=document.createElement("div");
+ titleElement.classList.add("title");
+ idElement.appendChild(titleElement);
+
+ const gameImageElement=document.createElement("div");
+ gameImageElement.classList.add("gameImage");
+ titleElement.appendChild(gameImageElement);
+
+ const imageElement=document.createElement("img");
+imageElement.src=Games;
+gameImageElement.appendChild(imageElement);
+
+const descriptionElement=document.createElement("div");
+descriptionElement.classList.add("description");
+listElement.appendChild(descriptionElement);
+
+
+});
+getGames.appendChild(listElement);
 }
-
-function setTypeFilter() {
-    var types = document.getElementsByName("type");
-    var selectedType;
-
-    for (var i = 0; i < types.length; i++){
-        if(types[i].checked){
-            selectedType = types[i].value;
-            break;
-        }
-    }
-    if (selectedType) {
-        baseUrl.searchParams.set("type", selectedType);
-    }
-}
-/*
-function setParticipantsFilter() {
-    // todo: get the input value and set the search paramater
-    var nrParticipants=document.getElementById("participants").value;
-if(nrParticipants){
-    baseUrl.searchParams.set("participants",nrParticipants);
-}
-}
-
-function setPriceFilter() {
-    // todo: get the inputs value and set the search paramater
-}
-
-function displayActivity(currentActivity) {
-    var activity = document.getElementById("activity");
-    activity.innerHTML = '';
-    var description = document.createElement("p");
-    description.innerText = "Description: " + currentActivity.activity;
-
-    var type = document.createElement("p");
-    type.innerText = "Type: " + currentActivity.type;
-
-    var participants = document.createElement("p");
-    participants.innerText = "Participants: " + currentActivity.participants;
-
-    var price = document.createElement("p");
-    price.innerText = "Price: " + currentActivity.price;
-
-    activity.appendChild(description);
-    activity.appendChild(type);
-    activity.appendChild(participants);
-    activity.appendChild(price);
-}
+getGames();
 
 
-function displayError(error) {
-    // todo: display the error
-var errorDiv=document.getElementById("error");
-errorDiv.innerText=error;
-}
+//varianta1 nu am reusit mai mult ....url imi da mereu eroare 404 not found nu stiu de ce....
+/*var baseUrl = new URL("https://games-world.herokuapp.com");
 
-function getPriceRange(price) {
-    // todo: convert the price which is between 0 - 1 into free, $, $$, $$$ sistem
-}
+function getGames() {
+    return fetch("https://games-world.herokuapp.com")
+      .then(function (response) {
+        return response.json()
+      })
+  }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("activity-button").addEventListener("click", getActivty);
-});*/
+fetch(baseUrl+ "games",{
+ method: 'GET'
+})
+.then(function(response){
+return response.json();
+})
+.then(function(jsonResp) {
+console.log(jsonResp);
+});
+
+
+const container = document.querySelector('#games');
+getGames().then (function (games) {
+  console.log(games)
+
+  for(let idx=0; idx < games.length; idx++){
+    const game = games[idx]; 
+    const gameDOM = createGameDOM(game);
+    container.appendChild(gameDOM);
+
+  }
+  getGames();
+  container.appendChild(gameDOM);
+})*/
+
