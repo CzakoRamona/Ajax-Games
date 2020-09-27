d24a61315a292b03c75c6e44a51199958d75411a
Ramona <drvizitiuramona@gmail.com>
2020-09-27 10:56:06 +0300

    modif varianta 2

--- games.js
+++ games.js
@@ -1,80 +1,75 @@
 
-//VARIANTA 2 aici imi da erore [object object]
-var baseUrl ="https://games-world.herokuapp.com/games";
+//VARIANTA 2 
+var baseUrl = "https://games-world.herokuapp.com/games";
 
 
 async function getGames() {
 
-let response=await fetch (baseUrl);
-let json=await response.json();
-console.log(json)
+  let response = await fetch(baseUrl);
+  let gameList = await response.json();
+  console.log(gameList)
 
 
-var GamesElement=document.querySelector(".games");
+  var gamesElement = document.querySelector(".games");
 
 
-//<div class="games">   
+  //<div class="games">   
 
-//<div class="list">
-//<div class="_id">id</div>
-    //<div class="title">title</div>
-        //<div class="gameImage">
-           // <img class="imageUrl" src= ""alt="game"></img>
-        //</div>
-   // <div class="description">Black hot d.....</div>
-//</div>
-//</div>
+  //<div class="list">
+  //<div class="_id">id</div>
+  //<div class="title">title</div>
+  //<div class="gameImage">
+  // <img class="imageUrl" src= ""alt="game"></img>
+  //</div>
+  // <div class="description">Black hot d.....</div>
+  //</div>
+  //</div>
 
-json.forEach( Games=> {
+  gameList.forEach(game => {
 
- const listElement=document.createElement("div");
- listElement.classList.add("list");
- GamesElement.appendChild(listElement);
+    const gameAll = gameDetails(game)
+    gamesElement.appendChild(gameAll)
+  });
 
- const idElement=document.createElement("div");
- idElement.classList.add("_id");
- GamesElement.appendChild(idElement);
-
- const titleElement=document.createElement("div");
- titleElement.classList.add("title");
- GamesElement.appendChild(titleElement);
-
- const gameImageElement=document.createElement("div");
- gameImageElement.classList.add("gameImage");
- GamesElement.appendChild(gameImageElement);
-
- const imageElement=document.createElement("img");
-imageElement.src=Games;
-gameImageElement.appendChild(imageElement);
-
-const descriptionElement=document.createElement("div");
-descriptionElement.classList.add("description");
-GamesElement.appendChild(descriptionElement);
-
-/*JSON.parse('{_id: "5f57a9bffd90830020fa5294", title: "Gran Turismo Sport™", imageUrl: "https://psmedia.playstation.com/is/image/psmedia/g…rt-two-column-01-ps4-',(key, value) => {
-  console.log(key); // log the current property name, the last is "".
-  return value;     // return the unchanged property value.
-});*/
-});
-
-return this.descriptionElement
 }
 getGames()
 
+function gameDetails(game) {
+  const gameAllDivs = document.createElement("div")
 
+  const titleElement = document.createElement("div");
+  titleElement.classList.add("title");
+  titleElement.innerHTML = game.title
+  gameAllDivs.appendChild(titleElement);
+
+  const imageElement = document.createElement("img");
+  imageElement.src = game.imageUrl;
+  gameAllDivs.appendChild(imageElement);
+
+  const descriptionElement = document.createElement("div");
+  descriptionElement.classList.add("description");
+  descriptionElement.innerHTML = game.description
+  gameAllDivs.appendChild(descriptionElement);
+  return gameAllDivs
+}
 
 
 //varianta1 nu am reusit mai mult ....url imi da mereu eroare 404 not found nu stiu de ce....
 /*var baseUrl = new URL("https://games-world.herokuapp.com");
 
-function getGames() {
+/*function getGames() {
     return fetch("https://games-world.herokuapp.com")
       .then(function (response) {
         return response.json()
       })
+  }*/
+
+/*  async function getGames(){
+    const response=await fetch("https://games-world.herokuapp.com");
+    return response.json()
   }
 
-fetch(baseUrl+ "games",{
+/*fetch(baseUrl+ "games",{
  method: 'GET'
 })
 .then(function(response){
@@ -82,20 +77,33 @@ return response.json();
 })
 .then(function(jsonResp) {
 console.log(jsonResp);
-});
+});*/
+//  async function getDetailsGames(){
+//    const response=await fetch("https://games-world.herokuapp.com/games",{method:"GET"})
+//    return response.json()
+//  }
+// async function main(){
+// const container = document.querySelector("#games");
 
+// try{
+//   const games=await getDetailsGames();
+//   console.log(games)
+// }
+// const gamesDOM=games
+// .map( games=>new Game(game.title,game.description))
+// .map(gameObject=> gameObject.render())
 
-const container = document.querySelector('#games');
-getGames().then (function (games) {
-  console.log(games)
+// gamesDOM.forEach( gamesDOM=>container.appendChild(gameDOM))
+//getDetailsGames().then (function (games) {
+//  console.log(games)
 
-  for(let idx=0; idx < games.length; idx++){
-    const game = games[idx]; 
-    const gameDOM = createGameDOM(game);
-    container.appendChild(gameDOM);
+// for(let idx=0; idx < games.length; idx++){
+//   const game = games[idx]; 
+//   const gamesDOM = createGameDOM("div");
+//   container.appendChild(gameDOM);
 
-  }
-  getGames();
-  container.appendChild(gameDOM);
-})*/
-
+//   }
+//   getDetailsGames();
+//   container.appendChild(gameDOM);
+// })
+// }
