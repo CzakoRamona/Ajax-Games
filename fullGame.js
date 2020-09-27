fd04ee251e68aea17f8432b7bb34f2fc149657ca
Ramona <drvizitiuramona@gmail.com>
2020-09-27 13:14:20 +0300

    varianta 3classes and obj

---
+++ fullGame.js
@@ -0,0 +1,21 @@
+class Game {
+    constructor(title, imageUrl, description) {
+        this.title = title
+        this.imageUrl = imageUrl
+        this.description = description
+    }
+    render() {
+        const gameDOM = document.createElement("div");
+        gameDOM.style.border = "2px solid white"
+
+        gameDOM.innerHTML = `
+        <h3>${this.title}</h3>
+        <p>${this.imageUrl}</p>
+        <p>${this.description}</p>`
+
+        return gameDOM;
+    }
+}
+async function getGames() {
+    const response = await fetch()
+}
